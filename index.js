f06ef343ed0653293156cfbc972c511611b38b26
Tom Siemion <tsiemion07@gmail.com>
2021-04-24 13:16:43 -0500

    working input validations

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const Intern = require('./lib/intern');
 const managerList = [];
 const engineerList = [];
 const internList = [];
+const idList = [];
+const emailList = [];
 // questions to ask if the user decides to add a new employee
 const employeeGenerateQuestions = [{
     name: 'position',
@@ -19,29 +21,83 @@ const employeeGenerateQuestions = [{
     name: 'employeeName',
     type: 'input',
     message: `What is the new employee's name?`,
+    // validation of the name entry, requires the name to be entered
+    validate: ((name) => { 
+        if (name !== '') {
+            return true;
+        } else {
+            return 'A name must be entered.'
+        }
+    })
 }, {
     name: 'employeeID',
     type: 'input',
-    message: `What is the new employee's ID number?`
+    message: `What is the new employee's ID number?`,
+    // validation to see if an employee ID has been entered already
+    validate: ((id) => {
+        if (id == '') return 'An employee ID must be entered.'
+        if (idList.includes(id)) {
+            return 'This ID already exists, enter a new Employee ID.';
+        } else {
+            return true;
+        }
+    })
 }, {
     name: 'employeeEmail',
     type: 'input',
-    message: `What is the new employee's email?`
+    message: `What is the new employee's email?`,
+    // validation to see if given unique email and fix email syntax
+    validate: ((email) => {
+        let regex = /\S+@\S+\.\S+/; // regex email format
+        if (regex.test(email)) { // if correct format is true
+            if (emailList.includes(email)) { // check if email is unique in email list
+                return 'This email already exists, enter a new unique email.';
+            } else {
+                return true;
+            }
+        } else {
+            return `Incorrect email format, follow ____@____.____`;
+        }
+    })
 }, {
     name: 'officeNumber',
     type: 'input',
     message: `What is the new employee's office number?`,
-    when: answers => answers.position === 'Manager'
+    when: answers => answers.position === 'Manager',
+    // validation for unique office number
+    validate: ((officeNum) => {
+        if (officeNum == '') return 'An office number must be entered.'
+        if (managerList.filter((mgr) => mgr.officeNumber === officeNum).length > 0) {
+            return `The office is taken, assign new office.`;
+        }
+        return true;
+    })
 }, {
     name: 'github',
     type: 'input',
     message: `What is the new employee's GitHub username?`,
-    when: answers => answers.position === 'Engineer'
+    when: answers => answers.position === 'Engineer',
+    // verifying a unique username was received
+    validate: ((gitUN) => {
+        if (gitUN == '') return 'A username must be entered.';
+        if (engineerList.filter((engi) => engi.github === gitUN).length > 0) {
+            return `The GitHub user ID already exists, enter the employees correct GitHub user ID.`;
+        }
+        return true;
+    })
 }, {
     name: 'school',
     type: 'input',
     message: `What school is the new employee attending?`,
-    when: answers => answers.position === 'Intern'
+    when: answers => answers.position === 'Intern',
+    //validating if something was entered for the school field
+    validate: ((sch) => {
+        if (sch == '') {
+            return 'A school must be entered.'
+        } else {
+            return true;
+        }
+    })
 }]
 // question to ask if the user wants to quit or make a new employee
 const continueOrQuitQuestion = {
@@ -99,6 +155,8 @@ function createNewEmployee() {
                         data.employeeID,
                         data.employeeEmail,
                         data.officeNumber));
+                    idList.push(data.employeeID); // for future validation
+                    emailList.push(data.employeeEmail) // for future validation
                     break;
                 // if selected employee type is engineer
                 case 'Engineer':
@@ -108,6 +166,8 @@ function createNewEmployee() {
                         data.employeeID,
                         data.employeeEmail,
                         data.github));
+                    idList.push(data.employeeID); // for future validation
+                    emailList.push(data.employeeEmail) // for future validation
                     break;
                 // if selected employee type is intern
                 case 'Intern':
@@ -117,6 +177,8 @@ function createNewEmployee() {
                         data.employeeID,
                         data.employeeEmail,
                         data.school));
+                    idList.push(data.employeeID); // for future validation
+                    emailList.push(data.employeeEmail) // for future validation
                     break;
             }
             // calls init function to see if user wants to continue to end app
